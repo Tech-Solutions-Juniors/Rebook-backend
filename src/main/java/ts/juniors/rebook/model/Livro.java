8f3565f6069a8dfd2f92bb8b040a08d60d153ef8
rommelbonfim <70414415+rommelbonfim@users.noreply.github.com>
2024-08-08 21:06:43 -0300

    entidade livro incompleta

---
+++ src/main/java/ts/juniors/rebook/model/Livro.java
@@ -0,0 +1,50 @@
+package ts.juniors.rebook.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ts.juniors.rebook.enums.Estados;
+import ts.juniors.rebook.enums.Generos;
+import java.util.List;
+
+@Entity
+@Table(name= "Livro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @NotBlank
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @ElementCollection(targetClass = Generos.class)
+    @CollectionTable(name = "Livro_Generos", joinColumns = @JoinColumn(name = "livro_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero")
+    @Size(max = 3, message = "Você pode escolher no máximo 3 gêneros")
+    private List<Generos> generos;
+
+    @ElementCollection(targetClass = Estados.class)
+    @CollectionTable(name = "Livro_Estados", joinColumns = @JoinColumn(name = "livro_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estados")
+    @Size(max = 1)
+    private List<Estados> estados;
+
+    @NotBlank
+    @Column(name = "autor", nullable = false)
+    private String autor;
+}
