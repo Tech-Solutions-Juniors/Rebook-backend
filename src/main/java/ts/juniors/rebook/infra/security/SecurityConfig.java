a01b56118c2e37b0d9d03d96b96be5c2e24d192b
rommelbonfim <70414415+rommelbonfim@users.noreply.github.com>
2024-08-22 20:06:56 -0300

    fix security filter

--- src/main/java/ts/juniors/rebook/infra/security/SecurityConfig.java
+++ src/main/java/ts/juniors/rebook/infra/security/SecurityConfig.java
@@ -3,14 +3,26 @@ package ts.juniors.rebook.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/**");
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll()  // Permite todas as requisições
+                )
+                .csrf(csrf -> csrf
+                        .disable()  // Desativa CSRF de forma não obsoleta
+                )
+                .headers(headers -> headers.frameOptions().disable());
+
+        return http.build();
     }
 }
